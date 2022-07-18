fc9094bf83645bcedddd4b9508fe25052cb2d8a0
MurylloEx <muryllopimenta@gmail.com>
2022-07-17 22:20:11 -0300

    test: made changes in config of unit and e2e tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,21 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   verbose: true,
+  preset: 'ts-jest',
+  moduleDirectories: ['src', 'node_modules'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  },
   moduleFileExtensions: [
     'js',
     'json',
     'ts'
   ],
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
+  rootDir: './',
+  modulePaths: ['<rootDir>'],
+  testRegex: '(.*\\.spec\\.ts|.*\\.e2e-spec\\.ts)$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest'
   },
