7b76cfa044385770a097d73e8084431421c58bad
MurylloEx <muryllopimenta@gmail.com>
2022-07-17 22:50:49 -0300

    ci: added new environment validation rules

--- src/config/schemas/root.schema.ts
+++ src/config/schemas/root.schema.ts
@@ -1,6 +1,8 @@
 import * as Joi from 'joi';
 
 export const EnvironmentSchema = Joi.object({
-  ENVIRONMENT: Joi.string().valid('development', 'production', 'local').default('local')
+  ENVIRONMENT: Joi.string()
+    .valid('development', 'production', 'local', 'testing')
+    .default('local')
 });
 
