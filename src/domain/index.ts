d63302e376e7cd2ee591f6e0b3a63942f519ad19
MurylloEx <muryllopimenta@gmail.com>
2022-07-30 04:17:23 -0300

    feat: finished domain base module and sample slice

---
+++ src/domain/index.ts
@@ -0,0 +1,3 @@
+export * from './exceptions';
+export * from './modules';
+export * from './slices';
---
+++ src/domain/modules/domain.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+import { MessageModule } from './message.module';
+
+@Module({
+  imports: [
+    MessageModule
+  ],
+  exports: [
+    MessageModule
+  ]
+})
+export class DomainModule {}
