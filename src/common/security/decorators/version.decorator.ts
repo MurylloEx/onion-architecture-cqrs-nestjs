b2c90fac1c5ec5f0bda2bbce6a47278390209f4c
MurylloEx <muryllopimenta@gmail.com>
2022-08-01 22:28:12 -0300

    feat: created permissions and version security decorators

--- src/common/security/decorators/version.decorator.ts
+++ src/common/security/decorators/version.decorator.ts
@@ -1,7 +1,7 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const Version = createParamDecorator((_, ctx: ExecutionContext) => {
+export const AppVersion = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest<Request>();
   return request.get('x-app-version') || '0.0.0';
 });
