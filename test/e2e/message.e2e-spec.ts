3a24decf1d153507cef70f10c50e387b697e17d1
MurylloEx <muryllopimenta@gmail.com>
2022-08-02 00:15:33 -0300

    test: created e2e (feature) test

---
+++ test/e2e/message.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Server } from 'http';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication, ModuleMetadata } from '@nestjs/common';
+import { ApiModule } from 'src/api/modules';
+
+const TESTING_MODULE: ModuleMetadata = {
+  imports: [ApiModule]
+};
+
+describe('MessageController (e2e)', () => {
+
+  let actualApp: INestApplication;
+  let actualServer: Server;
+
+  afterAll(() => actualApp.close());
+
+  beforeEach(async () => {
+    const actualModule = await Test.createTestingModule(TESTING_MODULE).compile();
+    actualApp = await actualModule.createNestApplication().init();
+    actualServer = actualApp.getHttpServer();
+  });
+
+  it('/message/all (GET)', async () => {
+    await request(actualServer)
+      .get('/message/all')
+      .expect(200);
+  });
+
+});
