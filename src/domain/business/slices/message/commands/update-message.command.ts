fbd222031109d0bad91e6e9ef27f9880449a539d
MurylloEx <muryllopimenta@gmail.com>
2022-08-07 00:53:04 -0300

    refactor: added Partial generic type to command arguments

--- src/domain/business/slices/message/commands/update-message.command.ts
+++ src/domain/business/slices/message/commands/update-message.command.ts
@@ -4,6 +4,6 @@ import { Message } from 'src/domain/business/slices/message/models';
 export class UpdateMessageCommand implements ICommand {
   constructor(
     public readonly id: string,
-    public readonly message: Message
+    public readonly message: Partial<Message>
   ) {}
 }
