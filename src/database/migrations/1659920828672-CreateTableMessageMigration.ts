82ff25d8c69cd20f45b48b9b2f7fb52afc0a04d7
MurylloEx <muryllopimenta@gmail.com>
2022-08-08 00:31:01 -0300

    feat: created sample of migration

---
+++ src/database/migrations/1659920828672-CreateTableMessageMigration.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateMessageTableMigration1659920828672 implements MigrationInterface {
+
+  async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+      CREATE TABLE IF NOT EXISTS 'message' (
+        'id'	varchar NOT NULL,
+        'createdAt'	datetime NOT NULL DEFAULT (datetime('now')),
+        'updatedAt'	datetime NOT NULL DEFAULT (datetime('now')),
+        'deletedAt'	datetime,
+        'title'	varchar NOT NULL,
+        'description'	varchar NOT NULL,
+        PRIMARY KEY('id')
+      );
+    `);
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DROP TABLE 'message'`);
+  }
+
+}
