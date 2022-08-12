0bb0af45dd571f00cb6a6eeafbff3d7b42f93221
MurylloEx <muryllopimenta@gmail.com>
2022-08-12 18:32:53 -0300

    feat: added UserService to ServicesModule

--- src/common/modules/services.module.ts
+++ src/common/modules/services.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { DomainModule } from 'src/domain';
-import { ConfigurationService, LoggingService, MessageService } from 'src/common/services';
+import { 
+  ConfigurationService, 
+  LoggingService, 
+  MessageService, 
+  UserService
+} from 'src/common/services';
 
 @Module({
   imports: [DomainModule],
   providers: [
     MessageService,
     ConfigurationService,
-    LoggingService
+    LoggingService,
+    UserService
   ],
   exports: [
     MessageService,
     ConfigurationService,
-    LoggingService
+    LoggingService,
+    UserService
   ]
 })
 export class ServicesModule {}
