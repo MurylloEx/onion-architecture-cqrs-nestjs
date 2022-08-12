4d20053b07bdd29fd4e4a7780d326c4b868f14a0
MurylloEx <muryllopimenta@gmail.com>
2022-08-12 18:14:44 -0300

    refactor: updated index exports

---
+++ src/domain/business/slices/user/index.ts
@@ -0,0 +1,8 @@
+export * from './commands';
+export * from './events';
+// export * from './exceptions';
+export * from './handlers';
+export * from './models';
+export * from './queries';
+export * from './repositories';
+export * from './services';
