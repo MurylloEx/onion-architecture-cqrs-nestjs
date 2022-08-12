1406c4ea85647fdd34c9af3d387d53ec3c050c86
MurylloEx <muryllopimenta@gmail.com>
2022-08-12 18:15:43 -0300

    feat: created UserModule for user domain slice

---
+++ src/domain/business/modules/user.module.ts
@@ -0,0 +1,44 @@
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { 
+  CreateUserHandler,
+  DeleteUserHandler,
+  FetchUserHandler, 
+  FetchOneUserHandler,
+  UpdateUserHandler,
+} from 'src/domain/business/slices/user';
+
+import { 
+  UserCreatedEventHandler, 
+  UserDeletedEventHandler, 
+  UserUpdatedEventHandler,
+} from 'src/domain/business/slices/user';
+
+import { 
+  User, 
+  UserDomainService, 
+  UserRepository,
+} from 'src/domain/business/slices/user';
+
+@Module({
+  imports: [
+    CqrsModule,
+    TypeOrmModule.forFeature([User])
+  ],
+  providers: [
+    CreateUserHandler,
+    FetchOneUserHandler,
+    FetchUserHandler,
+    UpdateUserHandler,
+    DeleteUserHandler,
+    UserCreatedEventHandler,
+    UserDeletedEventHandler,
+    UserUpdatedEventHandler,
+    UserRepository,
+    UserDomainService
+  ],
+  exports: [UserDomainService]
+})
+export class UserModule { }
