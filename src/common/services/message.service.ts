cf9b785244f38687cb96bfe32511bc986ada2c39
MurylloEx <muryllopimenta@gmail.com>
2022-08-12 18:17:25 -0300

    fix: missing "domain" word in property name

--- src/common/services/message.service.ts
+++ src/common/services/message.service.ts
@@ -5,26 +5,26 @@ import { MessageDomainService } from 'src/domain';
 @Injectable()
 export class MessageService {
 
-  constructor(private readonly messageService: MessageDomainService) {}
+  constructor(private readonly messageDomainService: MessageDomainService) {}
 
   create(title: string, description: string): Promise<MessageDto> {
-    return this.messageService.create(title, description);
+    return this.messageDomainService.create(title, description);
   }
 
   fetch(): Promise<MessageDto[]> {
-    return this.messageService.fetch();
+    return this.messageDomainService.fetch();
   }
 
   fetchOne(id: string): Promise<MessageDto> {
-    return this.messageService.fetchById(id);
+    return this.messageDomainService.fetchById(id);
   }
 
   update(id: string, message: MessageDto): Promise<MessageDto> {
-    return this.messageService.updateById(id, message);
+    return this.messageDomainService.updateById(id, message);
   }
 
   delete(id: string): Promise<MessageDto> {
-    return this.messageService.deleteById(id);
+    return this.messageDomainService.deleteById(id);
   }
 
 }
