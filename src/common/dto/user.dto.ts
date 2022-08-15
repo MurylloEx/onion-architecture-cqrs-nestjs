8f7be48e3e4ed69f74baf9818b938316e5590730
MurylloEx <muryllopimenta@gmail.com>
2022-08-15 00:15:37 -0300

    feat(dto): created user data transfer object

--- src/common/dto/user.dto.ts
+++ src/common/dto/user.dto.ts
@@ -1,37 +1,39 @@
-import { 
-  IsEmail, 
-  IsNumber, 
-  IsPhoneNumber, 
-  IsString 
-} from "class-validator";
+import {
+  IsDefined,
+  IsEmail,
+  IsNumber,
+  IsPhoneNumber,
+  IsString
+} from 'class-validator';
 
 export class UserDto {
 
   @IsString()
-  public sub: string;
-  
-  @IsNumber()
-  public iat: number;
-
-  @IsString()
+  @IsDefined()
   public fullName: string;
 
   @IsString()
+  @IsDefined()
   public nickName: string;
 
   @IsPhoneNumber('BR')
+  @IsDefined()
   public phone: string;
-  
+
   @IsEmail()
+  @IsDefined()
   public email: string;
-  
+
   @IsString()
+  @IsDefined()
   public password: string;
-  
+
   @IsString()
+  @IsDefined()
   public pushToken: string;
 
   @IsString()
+  @IsDefined()
   public pictureId: string;
 
 }
