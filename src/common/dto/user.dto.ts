d93ba4b8e7de0b8909533be865477554dcd1638c
Lucas Henrique <lucasmonte2011@gmail.com>
2022-08-15 10:02:25 -0300

    feat: added validators to User Dto

--- src/common/dto/user.dto.ts
+++ src/common/dto/user.dto.ts
@@ -1,36 +1,53 @@
+import { Exclude, Expose } from "class-transformer";
 import { 
   IsEmail, 
   IsNumber, 
   IsPhoneNumber, 
-  IsString 
+  IsString, 
+  Length, 
+  MinLength
 } from "class-validator";
 
+@Exclude()
 export class UserDto {
+
+  @IsString()
+  public id: string;
 
   @IsString()
   public sub: string;
   
+  @Exclude()
   @IsNumber()
   public iat: number;
 
+  @Expose()
   @IsString()
+  @Length(4, 64)
   public fullName: string;
 
+  @Expose()
   @IsString()
+  @Length(4, 32)
   public nickName: string;
 
+  @Expose()
   @IsPhoneNumber('BR')
   public phone: string;
   
+  @Expose()
   @IsEmail()
   public email: string;
   
   @IsString()
+  @MinLength(6)
   public password: string;
   
+  @Exclude()
   @IsString()
   public pushToken: string;
 
+  @Expose()
   @IsString()
   public pictureId: string;
 
