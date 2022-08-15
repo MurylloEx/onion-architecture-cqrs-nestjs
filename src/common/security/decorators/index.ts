fecdb1b95cc50373fbceef1181f1fec616c9e18f
MurylloEx <muryllopimenta@gmail.com>
2022-08-15 00:55:13 -0300

    refactor(security): made changes into User decorator to be Jwt decorator

--- src/common/security/decorators/index.ts
+++ src/common/security/decorators/index.ts
@@ -1,4 +1,4 @@
 export * from './security.decorator';
 export * from './version.decorator';
-export * from './user.decorator';
+export * from './jwt.decorator';
 export * from './permissions.decorator';
--- src/common/security/decorators/user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { Request } from 'express';
-
-export const User = createParamDecorator((_, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest<Request>();
-  return request['user'] ?? null;
-});
