dff19bfe96db12f46feae8c5e7a3c29a6af768b7
Lucas Henrique <lucasmonte2011@gmail.com>
2022-08-15 11:06:31 -0300

    feat: created dto to update User

---
+++ src/common/dto/update-user.dto.ts
@@ -0,0 +1,23 @@
+import { 
+  IsPhoneNumber, 
+  IsString, 
+  Length
+} from "class-validator";
+
+export class UpdateUserDto {
+
+  @IsString()
+  @Length(4, 64)
+  public fullName: string;
+
+  @IsString()
+  @Length(4, 32)
+  public nickName: string;
+
+  @IsPhoneNumber('BR')
+  public phone: string;
+
+  @IsString()
+  public pictureId: string;
+
+}
