1f02db4c6a880fb3cfcc66aaf812f5ad7ff99e4c
MurylloEx <muryllopimenta@gmail.com>
2022-08-14 23:08:52 -0300

    feat(domain): added BucketModule to BusinessDomainModule

--- src/domain/business/modules/business-domain.module.ts
+++ src/domain/business/modules/business-domain.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
+import { BucketModule } from './bucket.module';
 import { LoggingModule } from './logging.module';
 import { MessageModule } from './message.module';
 import { UserModule } from './user.module';
 
 @Module({
   imports: [
+    BucketModule,
     MessageModule,
     LoggingModule,
     UserModule
   ],
   exports: [
+    BucketModule,
     MessageModule,
     LoggingModule,
     UserModule
