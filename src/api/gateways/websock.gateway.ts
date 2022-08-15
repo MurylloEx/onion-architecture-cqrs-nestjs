a5faf28dd9c41ea3a76edb8376c66f314767a314
MurylloEx <muryllopimenta@gmail.com>
2022-08-15 11:17:10 -0300

    refactor: removed useless source in WebSockGateway

--- src/api/gateways/websock.gateway.ts
+++ src/api/gateways/websock.gateway.ts
@@ -1,30 +1,18 @@
 import { WebSocket } from 'ws';
-import { IncomingMessage } from 'http';
-import { MessageBody, OnGatewayInit } from '@nestjs/websockets';
+import { MessageBody } from '@nestjs/websockets';
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
-import { Jwt, JwtDto } from 'src/common';
+import { AuthorizeGuard, Jwt, JwtDto } from 'src/common';
+import { UseGuards } from '@nestjs/common';
 
+@UseGuards(AuthorizeGuard)
 @WebSocketGateway()
-export class WebSockGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+export class WebSockGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   protected clients: WebSocket[] = [];
-  protected messages: any[] = [];
-
-  afterInit(server: WebSocket) {
-    server.prependListener('connection', this.beforeConnection.bind(this));
-  }
-
-  beforeConnection(socket: WebSocket, request: IncomingMessage) {
-    socket['headers'] = request.headers;
-    //Do Jwt validation logic here
-  }
 
   handleConnection(client: WebSocket) {
     this.clients.push(client);
-    setTimeout(() => {
-      this.messages.slice(-50).forEach(m => client.send(JSON.stringify(m.data)));
-    }, 5000);
   }
   
   handleDisconnect(client: WebSocket) {
@@ -34,9 +22,7 @@ export class WebSockGateway implements OnGatewayConnection, OnGatewayDisconnect,
 
   @SubscribeMessage('broadcast')
   onBroadcast(@MessageBody() data: any, @Jwt() jwt: JwtDto) {
-    this.messages.push({ timestamp: +new Date, data });
-    this.messages.sort((a, b) => a.timestamp - b.timestamp);
-    this.clients.forEach(s => s.send(JSON.stringify(data)));
+    console.log('Message received: ', data);
   }
 
 }
