ac82f2cddf8d489c999c5f1203642bb7ce4f46ef
MurylloEx <muryllopimenta@gmail.com>
2022-08-16 03:55:30 -0300

    fix: involved fetchById call by a try/catch to throw domain exception

--- src/domain/business/slices/user/handlers/queries/fetch-one-user.handler.ts
+++ src/domain/business/slices/user/handlers/queries/fetch-one-user.handler.ts
@@ -3,6 +3,7 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { User } from 'src/domain/business/slices/user/models';
 import { FetchOneUserQuery } from 'src/domain/business/slices/user/queries';
 import { UserRepository } from 'src/domain/business/slices/user/repositories';
+import { UserNotFoundDomainException } from 'src/domain/business/slices/user/exceptions';
 
 @QueryHandler(FetchOneUserQuery)
 export class FetchOneUserHandler implements IQueryHandler<FetchOneUserQuery> {
@@ -10,7 +11,11 @@ export class FetchOneUserHandler implements IQueryHandler<FetchOneUserQuery> {
   constructor(private repository: UserRepository) {}
 
   async execute(query: FetchOneUserQuery): Promise<User> {
-    return await this.repository.fetchById(query.id);
+    try {
+      return await this.repository.fetchById(query.id);
+    } catch (error: any) {
+      throw new UserNotFoundDomainException();
+    }
   }
 
 }
