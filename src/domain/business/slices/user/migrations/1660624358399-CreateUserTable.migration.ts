f143eadb334fe647cd69b6066c8500cd2577ce95
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 01:32:05 -0300

    style: removed double quotes from user table migration

--- src/domain/business/slices/user/migrations/1660624358399-CreateUserTable.migration.ts
+++ src/domain/business/slices/user/migrations/1660624358399-CreateUserTable.migration.ts
@@ -5,17 +5,17 @@ export class CreateUserTableMigration1660624358399 implements MigrationInterface
   async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
       CREATE TABLE 'user' (
-        'id'	varchar NOT NULL,
-        'created_at'	datetime NOT NULL DEFAULT (datetime('now')),
-        'updated_at'	datetime NOT NULL DEFAULT (datetime('now')),
-        'deleted_at'	datetime,
-        'full_name'	varchar NOT NULL,
-        'nick_name'	varchar NOT NULL,
-        'phone'	varchar NOT NULL,
-        'email'	varchar NOT NULL,
-        'password'	varchar NOT NULL,
-        'push_token'	varchar NOT NULL,
-        'picture_id'	varchar NOT NULL,
+        'id' varchar NOT NULL,
+        'created_at' datetime NOT NULL DEFAULT (datetime('now')),
+        'updated_at' datetime NOT NULL DEFAULT (datetime('now')),
+        'deleted_at' datetime,
+        'full_name' varchar NOT NULL,
+        'nick_name' varchar NOT NULL,
+        'phone' varchar NOT NULL,
+        'email' varchar NOT NULL,
+        'password' varchar NOT NULL,
+        'push_token' varchar NOT NULL,
+        'picture_id' varchar NOT NULL,
         PRIMARY KEY('id')
       );
     `);
