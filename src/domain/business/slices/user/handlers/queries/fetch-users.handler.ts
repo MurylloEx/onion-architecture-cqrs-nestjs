db9024c2733f69cddcc2a7846f156a6f4bdc5cae
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 01:36:49 -0300

    fix: missing readonly keyword

--- src/domain/business/slices/user/handlers/queries/fetch-users.handler.ts
+++ src/domain/business/slices/user/handlers/queries/fetch-users.handler.ts
@@ -7,7 +7,7 @@ import { UserRepository } from 'src/domain/business/slices/user/repositories';
 @QueryHandler(FetchUsersQuery)
 export class FetchUserHandler implements IQueryHandler<FetchUsersQuery> {
 
-  constructor(private repository: UserRepository) {}
+  constructor(private readonly repository: UserRepository) {}
 
   async execute(query: FetchUsersQuery): Promise<User[]> {
     return await this.repository.fetch(query.options);
