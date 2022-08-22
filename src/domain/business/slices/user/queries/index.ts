e19f50ba1375d1eaa339253df151f49ee2eb79b0
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 01:26:37 -0300

    feat: added index export file

--- src/domain/business/slices/user/queries/index.ts
+++ src/domain/business/slices/user/queries/index.ts
@@ -1,2 +1,5 @@
 export * from './fetch-one-user.query';
 export * from './fetch-users.query';
+export * from './fetch-one-user-by-email.query';
+export * from './fetch-one-user-by-nick-name.query';
+export * from './verify-if-user-exists-by-email-or-nick-name.query';
