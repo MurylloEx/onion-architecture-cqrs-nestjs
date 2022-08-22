ec53dd88fd64cf47d30e9a061db867a8313a5977
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 01:27:56 -0300

    feat: added class validator decorators in UserModel

--- src/domain/business/slices/user/models/user.model.ts
+++ src/domain/business/slices/user/models/user.model.ts
@@ -1,28 +1,49 @@
-import { Column, Entity } from 'typeorm';
+import { IsPhoneNumber, IsString } from 'class-validator';
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
 import { DomainModel } from 'src/domain/models';
+import { Authentication, Confirmation, Recovery } from 'src/domain/business/slices/authentication';
 
 @Entity()
 export class User extends DomainModel {
 
+  @IsString()
   @Column()
   public fullName: string;
 
+  @IsString()
   @Column()
   public nickName: string;
 
+  @IsPhoneNumber('BR')
   @Column()
   public phone: string;
   
+  @IsString()
   @Column()
   public email: string;
   
+  @IsString()
   @Column()
   public password: string;
-  
+
+  @IsString()
   @Column()
   public pushToken: string;
 
+  @IsString()
   @Column()
   public pictureId: string;
+
+  @OneToOne(() => Confirmation, confirmation => confirmation.user)
+  @JoinColumn()
+  public confirmation: Confirmation;
+
+  @OneToMany(() => Recovery, recovery => recovery.user)
+  @JoinColumn()
+  public recoveries: Recovery[];
+
+  @OneToMany(() => Authentication, recovery => recovery.user)
+  @JoinColumn()
+  public authentications: Authentication[];
 
 }
