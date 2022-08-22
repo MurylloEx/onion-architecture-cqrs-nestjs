438960f340ce50c7806ef0293eaded59a0339081
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 02:39:33 -0300

    refactor: changed hardcoded version to use .env app version

--- src/common/interceptors/version.interceptor.ts
+++ src/common/interceptors/version.interceptor.ts
@@ -10,15 +10,22 @@ import {
   NestInterceptor
 } from '@nestjs/common';
 
+import { ConfigurationService } from 'src/common/services';
+
 @Injectable()
 export class VersionInterceptor implements NestInterceptor {
+
+  constructor(
+    protected readonly configurationService: ConfigurationService
+  ) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const ctx = context.switchToHttp();
     const request = ctx.getRequest<Request>();
 
     const clientVersion = request.get('x-app-version');
-    const clientRequiredVersion = '0.0.0'; //Change this version to dotenv MINIMUM_APP_VERSION
+
+    const clientRequiredVersion = this.configurationService.app.minimumVersion;
 
     if (clientVersion && compare(clientVersion, clientRequiredVersion, '>=')){
       return next.handle();
