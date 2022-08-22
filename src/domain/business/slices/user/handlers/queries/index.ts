9c61589841bb0628cda1a155ae25a0428c2b2644
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 01:36:27 -0300

    feat: added new query handlers to index export file

--- src/domain/business/slices/user/handlers/queries/index.ts
+++ src/domain/business/slices/user/handlers/queries/index.ts
@@ -1,2 +1,5 @@
 export * from './fetch-one-user.handler';
 export * from './fetch-users.handler';
+export * from './fetch-one-user-by-email.handler';
+export * from './fetch-one-user-by-nick-name.handler';
+export * from './verify-if-user-exists-by-email-or-nick-name.handler';
