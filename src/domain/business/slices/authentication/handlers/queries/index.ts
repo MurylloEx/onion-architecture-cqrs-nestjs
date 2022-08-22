7885a30baf36cb9981f85f78b7d82ce045c962f8
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 02:00:15 -0300

    feat: created index export file

---
+++ src/domain/business/slices/authentication/handlers/queries/index.ts
@@ -0,0 +1,7 @@
+export * from './check-recovery-code.handler';
+export * from './fetch-authentications.handler';
+export * from './fetch-confirmations.handler';
+export * from './fetch-recoveries.handler';
+export * from './fetch-one-authentication.handler';
+export * from './fetch-one-confirmation.handler';
+export * from './fetch-one-recovery.handler';
