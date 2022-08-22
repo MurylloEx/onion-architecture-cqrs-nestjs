bb938a5a7f774fb4f2cc702cf29b5c29d0ccec16
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 02:07:04 -0300

    feat: created command handler to authenticate user

---
+++ src/domain/business/slices/authentication/handlers/commands/authenticate-user.handler.ts
@@ -0,0 +1,55 @@
+import { compare } from 'bcrypt';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+import { UserDomainService } from 'src/domain/business/slices/user';
+import { Authentication } from 'src/domain/business/slices/authentication/models';
+import { JwtDomainService } from 'src/domain/business/slices/authentication/services';
+import { AuthenticateUserCommand } from 'src/domain/business/slices/authentication/commands';
+
+import { 
+  AccountNotConfirmedYetDomainException, 
+  InvalidUserCredentialsDomainException 
+} from 'src/domain/business/slices/authentication/exceptions';
+
+import { 
+  AuthenticationRepository, 
+  ConfirmationRepository 
+} from 'src/domain/business/slices/authentication/repositories';
+
+@CommandHandler(AuthenticateUserCommand)
+export class AuthenticateUserHandler implements ICommandHandler<AuthenticateUserCommand> {
+
+  constructor(
+    private readonly userDomainService: UserDomainService,
+    private readonly jwtDomainService: JwtDomainService,
+    private readonly authenticationRepository: AuthenticationRepository,
+    private readonly confirmationRepository: ConfirmationRepository
+  ) {}
+
+  async execute(command: AuthenticateUserCommand): Promise<Authentication> {
+    const user = await this.userDomainService.fetchByEmail(command.email);
+    const isPasswordValid = await compare(command.password, user.password);
+
+    if (!isPasswordValid) {
+      throw new InvalidUserCredentialsDomainException();
+    }
+
+    const isConfirmedAccount = await this.confirmationRepository.fetchByUserId(user.id);
+
+    if (!isConfirmedAccount) {
+      throw new AccountNotConfirmedYetDomainException();
+    }
+
+    const updatedUser = await this.userDomainService.updateById(user.id, {
+      pushToken: command.pushToken
+    });
+    const token = await this.jwtDomainService.sign(user.id, {...updatedUser});
+
+    return await this.authenticationRepository.create(
+      updatedUser,
+      command.ipAddress,
+      token,
+    );
+  }
+
+}
