33a0b7d097f9c385e68ab22895131a6ef0d33c29
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 02:14:33 -0300

    feat: created builder class for register user command

---
+++ src/domain/business/slices/authentication/commands/builders/register-user-command.builder.ts
@@ -0,0 +1,60 @@
+import { RegisterUserCommand } from 'src/domain/business/slices/authentication/commands';
+
+export class RegisterUserCommandBuilder {
+  
+  private fullName: string;
+  private nickName: string;
+  private phone: string;
+  private email: string;
+  private password: string;
+  private pushToken: string;
+  private pictureBuffer: Buffer;
+
+  withFullName(fullName: string): RegisterUserCommandBuilder {
+    this.fullName = fullName;
+    return this;
+  }
+
+  withNickName(nickName: string): RegisterUserCommandBuilder {
+    this.nickName = nickName;
+    return this;
+  }
+
+  withPhone(phone: string): RegisterUserCommandBuilder {
+    this.phone = phone;
+    return this;
+  }
+
+  withEmail(email: string): RegisterUserCommandBuilder {
+    this.email = email;
+    return this;
+  }
+
+  withPassword(password: string): RegisterUserCommandBuilder {
+    this.password = password;
+    return this;
+  }
+
+  withPushToken(pushToken: string): RegisterUserCommandBuilder {
+    this.pushToken = pushToken;
+    return this;
+  }
+
+  withPictureBuffer(pictureBuffer: Buffer): RegisterUserCommandBuilder {
+    this.pictureBuffer = pictureBuffer;
+    return this;
+  }
+
+  build(): RegisterUserCommand {
+    return new RegisterUserCommand(
+      this.fullName,
+      this.nickName,
+      this.phone,
+      this.email,
+      this.password,
+      this.pushToken,
+      this.pictureBuffer
+    );
+  }
+
+}
