87ed190488969e009445074c7e237948444ae8ef
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 01:25:44 -0300

    feat: added new methods to UserRepository

--- src/domain/business/slices/user/repositories/user.repository.ts
+++ src/domain/business/slices/user/repositories/user.repository.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, FindManyOptions, Repository, UpdateResult } from 'typeorm';
+import { FindManyOptions, Repository, UpdateResult } from 'typeorm';
 
 import { User } from 'src/domain/business/slices/user/models';
+import { UserNotFoundDomainException } from '../exceptions';
 
 @Injectable()
 export class UserRepository {
 
   constructor(
     @InjectRepository(User)
-    private repository: Repository<User>,
+    private readonly repository: Repository<User>,
   ) {}
 
   create(
@@ -34,11 +35,46 @@ export class UserRepository {
   }
 
   fetch(options?: FindManyOptions<User>): Promise<User[]> {
-    return this.repository.find(options);
+    try {
+      return this.repository.find(options);
+    } catch (error: any) {
+      throw new UserNotFoundDomainException();
+    }
   }
 
   fetchById(id: string): Promise<User> {
-    return this.repository.findOneByOrFail({ id });
+    try {
+      return this.repository.findOneByOrFail({ id });
+    } catch (error: any) {
+      throw new UserNotFoundDomainException();
+    }
+  }
+
+  fetchByEmail(email: string): Promise<User> {
+    try {
+      return this.repository.findOneByOrFail({ email });
+    } catch (error: any) {
+      throw new UserNotFoundDomainException();
+    }
+  }
+
+  fetchByNickName(nickName: string): Promise<User> {
+    try {
+      return this.repository.findOneByOrFail({ nickName });
+    } catch (error: any) {
+      throw new UserNotFoundDomainException();
+    }
+  }
+
+  async verifyIfUserExistsByEmailOrNickName(
+    email: string, 
+    nickName: string
+  ): Promise<boolean> 
+  {
+    return await this.repository.countBy([
+      { email },
+      { nickName }
+    ]) > 0;
   }
 
   updateById(id: string, user: Partial<User>): Promise<UpdateResult> {
