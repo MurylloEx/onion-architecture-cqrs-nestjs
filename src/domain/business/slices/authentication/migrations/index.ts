8c317a9c83337a72fa1357b4be412a906d45f86b
MurylloEx <muryllopimenta@gmail.com>
2022-08-22 01:56:12 -0300

    feat: created migrations index export file

---
+++ src/domain/business/slices/authentication/migrations/index.ts
@@ -0,0 +1,3 @@
+export * from './1660780461390-CreateTableAuthentication.migration';
+export * from './1660780443848-CreateTableConfirmation.migration';
+export * from './1660780424112-CreateTableRecovery.migration';
