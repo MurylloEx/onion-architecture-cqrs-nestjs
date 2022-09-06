df19b699e6bd49000ed8e851af90e526c27aba3c
MurylloEx <muryllopimenta@gmail.com>
2022-09-06 12:50:40 -0300

    feat: throwing custom exception

--- src/domain/business/slices/user/handlers/queries/fetch-one-user-by-email.handler.ts
+++ src/domain/business/slices/user/handlers/queries/fetch-one-user-by-email.handler.ts
@@ -3,6 +3,7 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { User } from 'src/domain/business/slices/user/models';
 import { UserRepository } from 'src/domain/business/slices/user/repositories';
 import { FetchOneUserByEmailQuery } from 'src/domain/business/slices/user/queries';
+import { UserNotFoundDomainException } from 'src/domain/business/slices/user/exceptions';
 
 @QueryHandler(FetchOneUserByEmailQuery)
 export class FetchOneUserByEmailHandler implements IQueryHandler<FetchOneUserByEmailQuery> {
@@ -10,7 +11,11 @@ export class FetchOneUserByEmailHandler implements IQueryHandler<FetchOneUserByE
   constructor(private readonly repository: UserRepository) {}
 
   async execute(query: FetchOneUserByEmailQuery): Promise<User> {
-    return await this.repository.fetchByEmail(query.email);
+    try {
+      return await this.repository.fetchByEmail(query.email);
+    } catch (error) {
+      throw new UserNotFoundDomainException();
+    }
   }
 
 }
