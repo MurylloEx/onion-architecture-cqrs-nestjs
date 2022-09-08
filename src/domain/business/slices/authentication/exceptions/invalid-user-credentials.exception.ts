0a2d5da28d48007cc6440387a1c876802953f587
MurylloEx <muryllopimenta@gmail.com>
2022-09-07 23:22:07 -0300

    fix: wrong semantic in error phrase fixed

--- src/domain/business/slices/authentication/exceptions/invalid-user-credentials.exception.ts
+++ src/domain/business/slices/authentication/exceptions/invalid-user-credentials.exception.ts
@@ -3,6 +3,6 @@ import { DomainException } from 'src/domain/exceptions';
 
 export class InvalidUserCredentialsDomainException extends DomainException {
   constructor() {
-    super('O endereço de e-mail ou a senha fornecido são inválidos.', HttpStatus.UNAUTHORIZED);
+    super('O endereço de e-mail ou senha fornecidos são inválidos.', HttpStatus.UNAUTHORIZED);
   }
 }
