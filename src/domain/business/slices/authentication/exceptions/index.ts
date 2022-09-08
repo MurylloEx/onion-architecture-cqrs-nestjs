16df6c6cf7a0754e5c1979c2813cf3239300b9d3
MurylloEx <muryllopimenta@gmail.com>
2022-09-07 22:45:45 -0300

    refactor: removed exception from authentication slice

--- src/domain/business/slices/authentication/exceptions/index.ts
+++ src/domain/business/slices/authentication/exceptions/index.ts
@@ -1,7 +1,6 @@
 export * from './authentication-not-found.exception';
 export * from './confirmation-not-found.exception';
 export * from './recovery-not-found.exception';
-export * from './user-already-registered.exception';
 export * from './invalid-user-credentials.exception';
 export * from './account-not-confirmed-yet.exception';
 export * from './invalid-recovery-code.exception';
