70abfe049f8829af38d7c1c3583f3bfc73ab3794
MurylloEx <muryllopimenta@gmail.com>
2022-09-07 22:14:10 -0300

    feat: changed all properties of UpdateUserDTO to be optional

--- src/common/dto/update-user.dto.ts
+++ src/common/dto/update-user.dto.ts
@@ -1,23 +1,37 @@
 import { 
+  IsDefined,
+  IsEmail,
+  IsOptional,
   IsPhoneNumber, 
   IsString, 
   Length
 } from 'class-validator';
 
+import { SafeBufferTransform } from 'src/common/security';
+
 export class UpdateUserDto {
 
   @IsString()
   @Length(4, 64)
+  @IsOptional()
   public fullName: string;
 
   @IsString()
+  @IsEmail()
+  @IsOptional()
+  public email: string;
+
+  @IsString()
   @Length(4, 32)
+  @IsOptional()
   public nickName: string;
 
   @IsPhoneNumber('BR')
+  @IsOptional()
   public phone: string;
 
-  @IsString()
-  public pictureId: string;
+  @IsOptional()
+  @SafeBufferTransform()
+  public pictureBuffer: Buffer;
 
 }
