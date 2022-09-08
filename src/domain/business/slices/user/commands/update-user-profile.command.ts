c54c374ba85b7c863023342bc0e6bac8b9f7498c
MurylloEx <muryllopimenta@gmail.com>
2022-09-07 23:16:42 -0300

    feat: created command to update user profile

---
+++ src/domain/business/slices/user/commands/update-user-profile.command.ts
@@ -0,0 +1,12 @@
+import { ICommand } from '@nestjs/cqrs';
+
+export class UpdateUserProfileCommand implements ICommand {
+  constructor(
+    public readonly id: string,
+    public readonly fullName?: string,
+    public readonly nickName?: string,
+    public readonly phone?: string,
+    public readonly email?: string,
+    public readonly pictureBuffer?: Buffer
+  ) {}
+}
