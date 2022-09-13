62adda5bd9693a4e2ebd4d6a01e75412d86b99ce
MurylloEx <muryllopimenta@gmail.com>
2022-09-13 02:53:04 -0300

    feat: created Pet entity model

---
+++ src/domain/business/slices/pet/models/pet.model.ts
@@ -0,0 +1,104 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import {
+  IsDefined,
+  IsInt,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+  MinLength
+} from 'class-validator';
+
+import { DomainModel } from 'src/domain/models';
+import { User } from 'src/domain/business/slices/user';
+
+@Entity()
+export class Pet extends DomainModel {
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  @Column()
+  public name: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  @Column()
+  public species: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  @Column()
+  public breed: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  @Column()
+  public place: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  @Column()
+  public color: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  @Column()
+  public sex: string;
+
+  @IsDefined()
+  @IsPositive()
+  @IsNumber()
+  @IsInt()
+  @Column()
+  public age: number;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(256)
+  @Column()
+  public description: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(256)
+  @Column()
+  public habits: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(256)
+  @Column()
+  public allergies: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  @Column()
+  public fears: string;
+
+  @IsString()
+  @IsDefined()
+  @Column()
+  public pictureId: string;
+
+  @IsDefined()
+  @ManyToOne(() => User, user => user.pets)
+  public user: User;
+
+}
