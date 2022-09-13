f53bada29971c7d2b4acc6f43bf6aa0529b3ba95
MurylloEx <muryllopimenta@gmail.com>
2022-09-13 02:11:03 -0300

    feat: created Pet DTOs

---
+++ src/common/dto/pet.dto.ts
@@ -0,0 +1,95 @@
+import { Exclude } from 'class-transformer';
+import { 
+  IsDefined, 
+  IsInt, 
+  IsNumber, 
+  IsPositive, 
+  IsString, 
+  IsUUID, 
+  MaxLength, 
+  MinLength 
+} from 'class-validator';
+
+import { UserDto } from './user.dto';
+
+export class PetDto {
+
+  @IsUUID('4')
+  @IsString()
+  @IsDefined()
+  public id: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  public name: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  public species: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  public breed: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  public place: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  public color: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  public sex: string;
+
+  @IsDefined()
+  @IsPositive()
+  @IsNumber()
+  @IsInt()
+  public age: number;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(256)
+  public description: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(256)
+  public habits: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(256)
+  public allergies: string;
+
+  @IsString()
+  @IsDefined()
+  @MinLength(4)
+  @MaxLength(32)
+  public fears: string;
+
+  @IsString()
+  @IsDefined()
+  public pictureId: string;
+
+  @Exclude()
+  public user: UserDto;
+
+}
