39dfac0b8d9cd56feeb693346782e47e594da968
MurylloEx <muryllopimenta@gmail.com>
2022-09-18 17:49:39 -0300

    feat: created establishment app service

--- src/common/services/index.ts
+++ src/common/services/index.ts
@@ -2,6 +2,7 @@ export * from './authentication.service';
 export * from './bucket.service';
 export * from './configuration.service';
 export * from './confirmation.service';
+export * from './establishment.service';
 export * from './message.service';
 export * from './logging.service';
 export * from './pet.service';
