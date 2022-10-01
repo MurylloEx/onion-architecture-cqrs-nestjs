55c729c7db6c3291b95088c9ca7b0c4983e4787a
MurylloEx <muryllopimenta@gmail.com>
2022-10-01 20:57:44 -0300

    fix: wrong method to delete pet fixed

--- src/api/controllers/pet.controller.ts
+++ src/api/controllers/pet.controller.ts
@@ -1,5 +1,5 @@
 import { ApiTags } from '@nestjs/swagger';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 import { PetService } from 'src/common/services';
 import { Security, Permissions, Jwt, JwtDto, CreatePetDto, Access, PetDto } from 'src/common';
@@ -12,24 +12,24 @@ export class PetController {
   constructor(private readonly petService: PetService) { }
 
   @Get('/me')
-  @Permissions(Access.FETCH_OWN_PETS)
+  @Permissions(Access.READ_OWN_PETS)
   fetchOwnPets(@Jwt() jwt: JwtDto): Promise<PetDto[]> {
-    return this.petService.fetchOwnPets(jwt.id);
+    return this.petService.fetchOwn(jwt.id);
   }
 
   @Post('/create')
   @Permissions(Access.CREATE_PET)
   createPet(@Jwt() jwt: JwtDto, @Body() body: CreatePetDto): Promise<PetDto> {
-    return this.petService.createPet(jwt.id, body);
+    return this.petService.create(jwt.id, body);
   }
 
-  @Get('/delete/:id')
+  @Delete('/delete/:id')
   @Permissions(
-    Access.FETCH_OWN_PETS, 
+    Access.READ_OWN_PETS, 
     Access.DELETE_PET
   )
   deletePet(@Param('id') petId: string): Promise<PetDto> {
-    return this.petService.deletePet(petId);
+    return this.petService.delete(petId);
   }
 
 }
