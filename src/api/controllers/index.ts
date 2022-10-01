ef36f95b2e76efbed8f68017adcb508aeecf21ca
MurylloEx <muryllopimenta@gmail.com>
2022-10-01 20:58:06 -0300

    feat: added FeedController to index export file

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -1,5 +1,6 @@
 export * from './authentication.controller';
 export * from './establishment.controller';
+export * from './feed.controller';
 export * from './message.controller';
 export * from './pet.controller';
 export * from './user.controller';
