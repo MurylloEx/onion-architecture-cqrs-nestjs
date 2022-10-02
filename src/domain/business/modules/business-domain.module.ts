bedcf9b384e3e83871168543c687921d953ba026
MurylloEx <muryllopimenta@gmail.com>
2022-10-02 01:25:59 -0300

    feat: added FeedModule to BusinessDomainModule

--- src/domain/business/modules/business-domain.module.ts
+++ src/domain/business/modules/business-domain.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthenticationModule } from './authentication.module';
 import { BucketModule } from './bucket.module';
 import { EstablishmentModule } from './establishment.module';
+import { FeedModule } from './feed.module';
 import { LoggingModule } from './logging.module';
 import { MessageModule } from './message.module';
 import { NotificationModule } from './notification.module';
@@ -13,6 +14,7 @@ import { UserModule } from './user.module';
     AuthenticationModule,
     BucketModule,
     EstablishmentModule,
+    FeedModule,
     LoggingModule,
     MessageModule,
     NotificationModule,
@@ -23,6 +25,7 @@ import { UserModule } from './user.module';
     AuthenticationModule,
     BucketModule,
     EstablishmentModule,
+    FeedModule,
     LoggingModule,
     MessageModule,
     NotificationModule,
