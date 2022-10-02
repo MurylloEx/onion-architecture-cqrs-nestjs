b825574d282ed5d5de3e9e7be026bf615f7f8b6a
MurylloEx <muryllopimenta@gmail.com>
2022-10-02 05:12:07 -0300

    feat: created index export files

---
+++ src/domain/business/slices/feed/commands/index.ts
@@ -0,0 +1,5 @@
+export * from './create-post.command';
+export * from './delete-post.command';
+export * from './report-post.command';
+export * from './update-post.command';
+export * from './builders';
---
+++ src/domain/business/slices/feed/exceptions/index.ts
@@ -0,0 +1,2 @@
+export * from './post-filter-rule-not-found.exception';
+export * from './post-not-found.exception';
